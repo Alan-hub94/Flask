342ee9c7c5cf5ce1e666ec768915bb1436d9be13
Alan <cereal9494@gmail.com>
2021-05-01 02:21:04 -0300

    ver2

--- homework1.py
+++ homework1.py
@@ -4,17 +4,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('list.html')
+    return render_template('list.html')#Creo un path para que me redireccione a la lista de lenguajes.
 
-@app.route("/lenguajes")
+@app.route("/lenguajes")#Creo un nuevo path para que me muestre la lista.
 def lenguajes():
     return render_template('lenguajes.html')
 
-@app.route("/about")
-def about():
-    return "<h1> hola </h1>"
+@app.route("/about")            #--
+def about():                    #   -->Esto nada que ver, era solo para practicar y para ver si funcionaba primero con esto
+    return "<h1> hola </h1>"    #--
 
 if __name__== "__main__":
-    app.run(debug=True)
+    app.run(debug=True) #para que no te desconectes del servidor y solo actualicecd la pagina, me refiero al "debug=True"
 
 
